4c7638a8b5e5c6457c5fbf91b8a06da5698946f3
Ivan Borba <mrborbaivan@gmail.com>
2022-06-27 22:24:11 -0300

    feature: created the getById method, also criptografated the password on the create method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entity/users.entity';
 import { UsersService } from './users.service';
 
 @ApiTags('users')
@@ -11,7 +13,23 @@ export class UsersController {
   @ApiOperation({
     summary: 'Lista todos os usuários',
   })
-  getAll() {
+  getAll(): Promise<User[]> {
     return this.usersService.getAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Lista usuário por id',
+  })
+  getById(@Param('id') id: string): Promise<User> {
+    return this.usersService.getById(id);
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: 'Cria um novo usuário',
+  })
+  create(@Body() dto: CreateUserDto): Promise<User> {
+    return this.usersService.create(dto);
   }
 }
