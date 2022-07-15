87ffe7b44a43deac05010374b9129f207bb2639c
Ivan Borba <mrborbaivan@gmail.com>
2022-07-15 19:59:34 -0300

    feature: added the query params to the products getAll

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   UseGuards,
+  Query,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -34,8 +35,8 @@ export class ProductsController {
   @ApiOperation({
     summary: 'Listagem de produtos',
   })
-  findAll(): Promise<Product[]> {
-    return this.productsService.findAll();
+  findAll(@Query() query: Partial<Product>): Promise<Product[]> {
+    return this.productsService.findAll(query);
   }
 
   @Get(':id')
